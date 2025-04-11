4dfe5ffcf7d7fc3bab9432b0c1fffc8db8980c89
utso <utsosarkar7870@gmail.com>
2025-04-11 16:18:51 +0600

    home page code update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import About from "@/components/about/About";
 import Mission from "@/components/about/Mission";
 import BannerSlider from "@/components/Banner";
 import ClinicInfo from "@/components/ClinicInfo";
+import Insurance from "@/components/services/Insurance";
+import Service from "@/components/services/Service";
+import UrgentCare from "@/components/services/UrgentCare";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <ClinicInfo/>
       <About/>
       <Mission/>
+      <Service/>
+      <UrgentCare/>
+      <Insurance/>
     </div>
   );
 }
---
+++ components/services/Insurance.tsx
@@ -0,0 +1,16 @@
+import H2 from "../reusable/H2"
+
+function Insurance() {
+  return (
+    <section className=' max-w-[82.5rem] mx-auto mt-14'>
+      <div className=' bg-bgcolorColor rounded-2xl py-12'>
+         <div className=" max-w-xl mx-auto text-center space-y-4 px-4">
+            <H2>Accepted Insurance</H2>
+            <p className=" text-pragraphColor ">At North ave Clinic, We accept a wide range of insurance plans to make quality healthcare accessible and affordable for you.</p>
+         </div>
+      </div>
+    </section>
+  )
+}
+
+export default Insurance
